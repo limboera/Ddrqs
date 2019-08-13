c8bdb2d22a9ea118ea0c6980b1241288a675e7fd
limboera <53666421+limboera@users.noreply.github.com>
2019-08-13 08:06:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "424978283944345601") return;
+  if (message.author.id !== "610608645754978354") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
